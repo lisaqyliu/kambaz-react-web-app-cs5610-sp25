b771f80b7e30851194aaa5ea2f6b2698562f317c
lisaliu <lisaliu9194@gmail.com>
2025-01-14 14:39:54 -0800

    Lab1

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -29,20 +29,33 @@ export default function Lab1() {
                     add vertical spacing, wrap the paragraphs you
                     want to separate with the paragraph tag
                 </p>
-                <p>
+                <p id="wd-p-2">
                 This is the first paragraph. The paragraph tag is used to format
                 vertical gaps between long pieces of text like this one.
                 </p>
-                <p>
+                <p id="wd-p-3">
                     This is the second paragraph. Even though there is a deliberate
                     white gap between the paragraph above and this paragraph, by default
                     browsers render them as one contiguous piece of text as shown here on
                     the right.
                     </p>
-                    <p>
+                    <p id="wd-p-4">
                         This is the third paragraph. Wrap each paragraph with the
                         paragraph tag to tell browsers to render the gaps.
                     </p>
+            </div>
+            <div id="wd-lists">
+                <h4>List Tags</h4>
+                <h5>Ordered List Tag</h5>
+                How to make pancakes:
+                1. Mix dry ingredients.
+                2. Add wet ingredients.
+                3. Stir to combine.
+                4. Heat a skillet or griddle.
+                5. Pour batter onto the skillet.
+                6. Cook until bubbly on top.
+                7. Flip and cook the other side.
+                8. Serve and enjoy!
             </div>
         </div>
     );
