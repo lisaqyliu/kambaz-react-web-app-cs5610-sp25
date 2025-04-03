ea7b93edbb452ed43e57326ff2e68eb10ab7195f
lisaliu <lisaliu9194@gmail.com>
2025-04-02 20:49:06 -0700

    update a5

--- src/Kambaz/Account/client.ts
+++ src/Kambaz/Account/client.ts
@@ -1,11 +1,14 @@
 import axios from "axios";
+import.meta.env.VITE_REMOTE_SERVER
+
 
 const axiosWithCredentials = axios.create({
   withCredentials: true,
 });
 
 export const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
-export const USERS_API = `/api/users`;
+export const USERS_API = `${import.meta.env.VITE_REMOTE_SERVER}/api/users`;
+
 
 
 export const signin = async (credentials: any) => {
